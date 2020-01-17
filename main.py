ce4ac9ae96f4449625c9aa77d188b4844f0c9a2d
tbrowndev <46692709+tbrowndev@users.noreply.github.com>
2020-01-17 01:54:42 -0600

    Init

---
+++ main.py
@@ -0,0 +1,18 @@
+from progress.bar import Bar
+from riddle import Riddle
+import time
+
+_riddle = Riddle(key=27)
+_exposedData = input("Enter Text to Encrypt: ")
+_encryptedText = None
+_encryptionStatus = "ENCRYPTING"
+
+bar = Bar('Encrypting', max=20)
+for i in range(20):
+    time.sleep(.25)
+    bar.next()
+bar.finish()
+_encryptedText = _riddle.encrypt(obj=_exposedData)
+
+print("\nEncrypted Data:\n\n",_encryptedText,"\n")
+input('Press ENTER to Continue')
